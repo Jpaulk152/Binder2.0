1866f905c30a3f87ebf0ea102087e1f3a7829ad1
Revith <Jpaulk152@gmail.com>
2024-04-18 16:22:10 -0500

    Update SQLConfig.php

--- src/Models/DB/SQLConfig.php
+++ src/Models/DB/SQLConfig.php
@@ -4,9 +4,9 @@ namespace Models\DB;
 
 class SQLConfig{
 
-    public static $serverName = "10.68.68.96";
-    public static $databaseName = "phpdb";
-    public static $userName = "joey";
-    public static $password = "Tampered0182";
+    public static $serverName = "";
+    public static $databaseName = "";
+    public static $userName = "";
+    public static $password = "";
 
 }
